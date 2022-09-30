c35f4ae2b06f6a4e05b1bb23b551018608218862
Austin Higgins <ahiggins.dev@gmail.com>
2022-09-30 11:01:43 -0400

    Simple API final.

--- main.go
+++ main.go
@@ -1,16 +1,19 @@
 package main
 
 import(
+	"log"
+	"net/http"
+	"encoding/json"
 	"github.com/gorilla/mux"
 )
 
 func main() {
 	router := mux.NewRouter()
-	
-	router.HandleFunc("/books", func (w http.ResponseWriter, r *http.Request) {
+
+	router.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
 		json.NewEncoder(w).Encode("Hello World")
 	})
 
 	log.Println("API is running on 4040")
-	http.ListenandServe(":4040", router)
+	http.ListenAndServe(":4040", router)
 }
